a46cf9a9d858cdbfcc38df229bb9e913f00d776d
JK <kartikjraj@gmail.com>
2023-01-21 07:44:13 +0530

    Added average function

--- calculator.py
+++ calculator.py
@@ -15,10 +15,14 @@ def mult(a, b):
     z = a * b
     return z
 
+def average():
+    v = sum1(a, b)/2
+    return v
 
 lst = sys.argv
 a = int(lst[1])
 b = int(lst[2])
-print("The Sum of two numbers is", sum1(a, b))
-print("The Subtracted of two numbers is", sub(a, b))
-print("The Multiplied value of two numbers is", mult(a, b))
+print("The Sum of two numbers is ", sum1(a, b))
+print("The Subtracted of two numbers is ", sub(a, b))
+print("The Multiplied value of two numbers is ", mult(a, b))
+print("The Average of two numbers is ", average())
